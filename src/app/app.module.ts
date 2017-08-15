6162102367c99ba4c133a40174c0e3a039211668
Pawarut Klai-armon <pawarut.klaiarmon@gmail.com>
2017-08-15 16:25:34 +0700

    Import ng2-materialize to app module ;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,17 @@
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+/**
+ * Third-Party
+ */
+import { MaterializeModule } from 'ng2-materialize';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +19,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CoreModule,
+    SharedModule,
+    MaterializeModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
