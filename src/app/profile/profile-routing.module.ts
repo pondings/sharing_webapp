9cc386e71108082a3a3ebba734102194cf97ea4d
Pawarut Klai-armon <pawarut.klaiarmon@gmail.com>
2017-08-15 18:02:12 +0700

    Add router

--- src/app/profile/profile-routing.module.ts
+++ src/app/profile/profile-routing.module.ts
@@ -1,7 +1,10 @@
+import { ProfileComponent } from './profile.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'profile', component: ProfileComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,3 +1,4 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -7,7 +8,8 @@ import { ProfileComponent } from './profile.component';
 @NgModule({
   imports: [
     CommonModule,
-    ProfileRoutingModule
+    ProfileRoutingModule,
+    RouterModule
   ],
   declarations: [ProfileComponent]
 })
