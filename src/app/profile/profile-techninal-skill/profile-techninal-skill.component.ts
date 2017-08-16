276fc2274b104aed1d27cb51cbec95917ded2026
Pawarut Klai-armon <pawarut.klaiarmon@gmail.com>
2017-08-16 15:52:05 +0700

    Add profile tectnical skill component

---
+++ src/app/profile/profile-techninal-skill/profile-techninal-skill.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-techninal-skill',
+  templateUrl: './profile-techninal-skill.component.html',
+  styleUrls: ['./profile-techninal-skill.component.scss']
+})
+export class ProfileTechninalSkillComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
