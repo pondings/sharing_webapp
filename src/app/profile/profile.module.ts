a7f2cb447c0e884bcb83802efa72a4d13ace8785
Pawarut Klai-armon <pawarut.klaiarmon@gmail.com>
2017-08-16 15:52:40 +0700

    Add content

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -2,15 +2,20 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MaterializeModule } from 'ng2-materialize';
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
+import { ProfileEducationComponent } from './profile-education/profile-education.component';
+import { ProfileTechninalSkillComponent } from './profile-techninal-skill/profile-techninal-skill.component';
+import { ProfileExperienceComponent } from './profile-experience/profile-experience.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ProfileRoutingModule,
-    RouterModule
+    RouterModule,
+    MaterializeModule
   ],
-  declarations: [ProfileComponent]
+  declarations: [ProfileComponent, ProfileEducationComponent, ProfileTechninalSkillComponent, ProfileExperienceComponent]
 })
 export class ProfileModule { }
