86c32a9249cb7d6d96d42aa8d356ea6470b7c2ba
Pawarut Klai-armon <pawarut.klaiarmon@gmail.com>
2017-08-16 13:01:00 +0700

    Add model project

---
+++ src/app/shared/models/Project.ts
@@ -0,0 +1,12 @@
+
+export class Project {
+
+    public id: number;
+    public name: string;
+    public language: string;
+    public framework: string;
+    public demoLink: string;
+    public githubLink: string;
+    public desc: string;
+
+}
